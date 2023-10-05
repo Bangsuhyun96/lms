3156e96737714ba76b06ff64e350e838d58625ba
bangsuhyun <bangt9687@naver.com>
2023-10-05 16:51:22 +0900

    231005 - 관리자 페이지 생성

--- src/main/java/com/lms/admin/controller/AdminController.java
+++ src/main/java/com/lms/admin/controller/AdminController.java
@@ -7,10 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
     @GetMapping
     public String AdminHome(){
         return "admin/admin";
+    }
+
+    @GetMapping("/lwm")
+    public String LectureWeekManagement(){
+        return "admin/admin_lwm";
+    }
+
+    @GetMapping("/csm")
+    public String ClassScheduleManagement(){
+        return "admin/admin_csm";
+    }
+
+    @GetMapping("/moli")
+    public String ManagingOpenLectureInformation(){
+        return "admin/admin_moli";
     }
 
 }
