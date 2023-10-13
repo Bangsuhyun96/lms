9436b00c9ab1f83aecac935af7d4c655531853d7
Kimhoongyu <khun019@naver.com>
2023-10-13 18:36:39 +0900

    231013 - 과제 페이지 기능

--- src/main/java/com/lms/admin/assignments/service/AssignmentServiceImpl.java
+++ src/main/java/com/lms/admin/assignments/service/AssignmentServiceImpl.java
@@ -2,22 +2,25 @@ package com.lms.admin.assignments.service;
 
 import com.lms.admin.assignments.dto.AssignmentDto;
 import com.lms.admin.assignments.mapper.AssignmentMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class AssignmentServiceImpl implements AssignmentService {
-    private final AssignmentMapper assignmentMapper;
 
-    @Autowired
-    public AssignmentServiceImpl(AssignmentMapper assignmentMapper) {
-        this.assignmentMapper = assignmentMapper;
-    }
+    private final AssignmentMapper assignmentMapper;
 
     @Override
     public List<AssignmentDto> getAllAssignments() {
-        return assignmentMapper.selectAllAssignments();
+        return assignmentMapper.getAllAssignments();
+    }
+
+    @Override
+    public List<AssignmentDto> searchAssignmentsByYearAndSemester(String lectureYear, int curriculumSemester) {
+        return assignmentMapper.searchAssignmentsByYearAndSemester(lectureYear, curriculumSemester);
     }
 }
