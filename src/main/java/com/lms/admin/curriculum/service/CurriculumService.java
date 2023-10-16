7fa56565e13cda41c1526350d581c21c9b22066f
Sim-MinA <tlaalsdk7@naver.com>
2023-10-16 12:11:02 +0900

    교육과정정보 조회 추가

--- src/main/java/com/lms/admin/curriculum/service/CurriculumService.java
+++ src/main/java/com/lms/admin/curriculum/service/CurriculumService.java
@@ -7,5 +7,11 @@ import java.util.List;
 public interface CurriculumService {
 
     // 교육과정 목록 출력하기
-    List<CurriculumDto> getAllCurriculum();
+    List<CurriculumDto> selectCurriculum();
+
+    // 교육과정 추가하기
+    public void insertCurriculum(CurriculumDto curriculumDto);
+
+    // 교육과정 삭제하기
+    public void deleteCurriculum(List<Integer> curriculumIds);
 }
