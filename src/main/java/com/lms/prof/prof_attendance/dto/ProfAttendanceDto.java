799537b2dc178da5367e6a2082e785de9dcf94e7
youngseo <rldyaldudtj@naver.com>
2024-01-11 19:20:15 +0900

    교수 출석 (일단 .. 세미완성 )

--- src/main/java/com/lms/prof/prof_attendance/dto/ProfAttendanceDto.java
+++ src/main/java/com/lms/prof/prof_attendance/dto/ProfAttendanceDto.java
@@ -6,15 +6,26 @@ import java.time.LocalDate;
 
 @Data
 public class ProfAttendanceDto {
-    // 강의정보 lecture_info
-    private String lectureYear; // 강의년도
-    private int instructorNo; // 강사번호
-    private String lectureName; // 강의이름
+    // 강의 정보 lecture_info
+    private String lectureYear; // 강의 년도
+    private int instructorNo; // 강사 번호
+    private String lectureName; // 강의 이름
     private int profId;
-    private int lectureId; // 강의번호
+    private int lectureId; // 강의 번호
 
-    // 교육과정정보 curriculum
-    private String curriculumName; // 교육과정명
-    private LocalDate startDate;  // 시작일자
-    private LocalDate endDate;  // 종료일자
+    // 교육 과정 정보 curriculum
+    private String curriculumName; // 교육 과정명
+    private LocalDate startDate;  // 시작 일자
+    private LocalDate endDate;  // 종료 일자
+
+    // weekInfo 에 필요한 애들
+    private int lectureWeek; // 강의 주차
+    private LocalDate lectureDate; // 강의 날짜
+    private int count; // 인원수
+    private String name; // 학생 이름
+    private String email; // 학생 이메일
+    private int studentId; // 학생 번호
+    private int weekId; // 주차 번호
+    private String division; // 출결 상태
+    private int attendance; // 출석률
 }
